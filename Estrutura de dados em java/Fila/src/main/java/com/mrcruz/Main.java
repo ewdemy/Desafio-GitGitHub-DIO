cb28e02d1b7788259b9bed947f70b9cc94ee2c31
ewdemy <ewdemy@hotmail.com>
2023-02-08 21:56:15 -0300

    refatorando para embutir o nó

--- Estrutura de dados em java/Fila/src/main/java/com/mrcruz/Main.java
+++ Estrutura de dados em java/Fila/src/main/java/com/mrcruz/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         Fila fila = new Fila();
-        fila.enqueue(new No("No 1"));
-        fila.enqueue(new No("No 2"));
-        fila.enqueue(new No("No 3"));
-        fila.enqueue(new No("No 4"));
+        fila.enqueue("No 1");
+        fila.enqueue("No 2");
+        fila.enqueue("No 3");
+        fila.enqueue("No 4");
 
         System.out.println(fila);
         System.out.println(fila.dequeue());
         System.out.println(fila);
-        fila.enqueue(new No("Último"));
+        fila.enqueue("Último");
         System.out.println(fila);
         System.out.println(fila.first());
     }
